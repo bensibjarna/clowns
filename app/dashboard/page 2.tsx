aba462e11083ca0539e01975c32073fec66d67d7
bensioz <116746227+bensibjarna@users.noreply.github.com>
2023-12-27 17:31:45 +0000

    commit

--- app/dashboard/page 2.tsx
+++ app/dashboard/page 2.tsx
@@ -1,20 +1,19 @@
-"use client";
+// "use client";
 
-import { signOut } from "next-auth/react";  //
-import { Button } from "@/components/ui/button";
+// import { signOut } from "next-auth/react";  //
+// import { Button } from "@/components/ui/button";
 
+// export default function Home() {
+//     const handleSignOut = () => {
+//         signOut({
+//             callbackUrl: "/",
+//         });
+//     }
 
-export default function Home() {
-    const handleSignOut = () => {
-        signOut({
-            callbackUrl: "/",
-        });
-    }
-
-    return (
-        <div>
-            <h1>Dashboard</h1>
-            <Button onClick={handleSignOut}>Sign Out</Button>
-        </div>
-    )
-}
+//     return (
+//         <div>
+//             <h1>Dashboard</h1>
+//             <Button onClick={handleSignOut}>Sign Out</Button>
+//         </div>
+//     )
+// }
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,20 +1,37 @@
+// app/dashboard/page.tsx
 "use client";
 
-import { signOut } from "next-auth/react";  //
-import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import { signOut } from "next-auth/react";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 
+export default function Dashboard() {
+	const handleSignOut = () => {
+		signOut({
+			callbackUrl: "/",
+		});
+	};
 
-export default function Home() {
-    const handleSignOut = () => {
-        signOut({
-            callbackUrl: "/",
-        });
-    }
-
-    return (
-        <div>
-            <h1>Dashboard</h1> 
-            <Button onClick={handleSignOut}>Sign Out</Button>
-        </div>
-    )
+	return (
+		<div className='flex flex-col items-center justify-center min-h-screen p-4'>
+			<h1 className='text-2xl font-bold mb-4'>Dashboard</h1>
+			<div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
+				<Link href='/events/pobba-rolt-2023'>
+					<a className='block'>
+						<Card>
+							<CardHeader>
+								<CardTitle>Pöbba Rölt 2023</CardTitle>
+							</CardHeader>
+							<CardContent>
+								<p>Join the exciting Pöbba Rölt 2023 event!</p>
+							</CardContent>
+						</Card>
+					</a>
+				</Link>
+			</div>
+			<button onClick={handleSignOut} className='mt-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600'>
+				Sign Out
+			</button>
+		</div>
+	);
 }
